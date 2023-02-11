a53c29d466fb68e1a354ce367eff009457ba3c3c
noonan-122 <noonan.122@osu.edu>
2023-02-11 16:07:32 -0500

    Added build and edit content files
    MacOS users need to have these files so they can build and edit with mgcb.

---
+++ sprint0/build-content.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PATH=/usr/local/share/dotnet/x64:$PATH
+dotnet build
+
---
+++ sprint0/edit-content.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export PATH=/usr/local/share/dotnet/x64:$PATH
+dotnet mgcb-editor Content/Content.mgcb
+
+
