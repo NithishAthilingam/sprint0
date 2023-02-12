58cfbfc2eebbac9642f825100fa643b98bfed771
Nithish Athilingam <nithish.athilingam@gmail.com>
2023-02-12 16:50:00 -0500

    added load rect file

---
+++ sprint0/LoadRectangles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+//using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using sprint0;
+//using Microsoft.Xna.Framework.Net;
+//using Microsoft.Xna.Framework.Storage;
+
+namespace sprint0
+{
+    public class LoadRectangles
+    {
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Blue);
+            Rectangle source1 = new Rectangle(0, 0, 20, 20);
+            Rectangle dest1 = new Rectangle(100, 100, 40, 40);
+
+            Rectangle source2 = new Rectangle(0, 1, 20, 20);
+            Rectangle dest2 = new Rectangle(100, 100, 40, 40);
+            spriteBatch.Begin();
+
+            sprite.Draw(spriteBatch, Animate);
+
+            TextSprite.Draw(spriteBatch, font);
+
+            if (currentFrame == 0)
+            {
+                spriteBatch.Draw(zelda, dest1, source1, Color.White);
+            }
+            else if (currentFrame == 5)
+            {
+                spriteBatch.Draw(zelda, dest2, source2, Color.White);
+            }
+            spriteBatch.Draw(zelda, dest1, source1, Color.White);
+
+            base.Draw(gameTime);
+            spriteBatch.End();
+        }
+    }
+}
