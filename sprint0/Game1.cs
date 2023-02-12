d632aa47c71dbe78699c8566df347a3e3079856e
Afrah Taj <afrah.taj22@gmail.com>
2023-02-12 15:08:19 -0500

    Updated Game1.cs
    Has source and destination rectangles.

--- sprint0/Game1.cs
+++ sprint0/Game1.cs
@@ -26,7 +26,11 @@ namespace sprint0
         private Texture2D spriteB;
         private Texture2D spriteC;
         //hello testing to see if i can commit
-        
+        private Texture2D zelda;
+        private Texture2D charcater;
+        private int currentFrame = 0;
+        private int totalFrames = 10;
+        private Vector2 location = new Vector2(220, 100);
         //:))))
         private SpriteFont font;
         private Isprite TextSprite;
@@ -59,6 +63,7 @@ namespace sprint0
             spriteC = Content.Load<Texture2D>("d");
             Animate[3] = spriteC;
             sprite = new RSprite();
+            zelda = Content.Load<Texture2D>("sprites-link");
 
             font = Content.Load<SpriteFont>("Score");
             TextSprite = new TextSprite();
@@ -81,17 +86,38 @@ namespace sprint0
                 controller.Update();
             }
 
+            currentFrame++;
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Blue);
+            Rectangle source1 = new Rectangle(0, 0, 20, 20);
+            Rectangle dest1 = new Rectangle(100, 100, 40, 40);
+
+            Rectangle source2 = new Rectangle(0, 1, 20, 20);
+            Rectangle dest2 = new Rectangle(100, 100, 40, 40);
             spriteBatch.Begin();
 
             sprite.Draw(spriteBatch, Animate);
 
             TextSprite.Draw(spriteBatch, font);
+
+            if (currentFrame == 0)
+            {
+                spriteBatch.Draw(zelda, dest1, source1, Color.White);
+            }
+            else if (currentFrame == 5)
+            {
+                spriteBatch.Draw(zelda, dest2, source2, Color.White);
+            }
+            spriteBatch.Draw(zelda, dest1, source1, Color.White);
 
             base.Draw(gameTime);
             spriteBatch.End();
