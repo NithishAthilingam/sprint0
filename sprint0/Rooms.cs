2d29ea9c6cca4ee01bdb5bc7595cc75b1115368c
Afrah Taj <afrah.taj22@gmail.com>
2023-03-05 22:00:35 -0500

    rooms

--- sprint0/Rooms.cs
+++ sprint0/Rooms.cs
@@ -7,16 +7,14 @@ namespace sprint0
 	public class Rooms
 	{
 
-      
-        Rectangle roomBorder;
-        Rectangle leftDoorPos;
-        Rectangle topDoorPos;
-        Rectangle bottomDoorPos;
-        Rectangle rightDoorPos;
+        Rectangle source2;
+        Rectangle source1;
+        Rectangle source3;
+        Rectangle source4;
+        Rectangle source5;
+        Rectangle source6;
         private Texture2D room;
         private Game1 game1;
-
-        Rectangle source2;
 
 
 
@@ -26,13 +24,7 @@ namespace sprint0
 
             room = r;
 
-            roomBorder = new Rectangle(521, 11, 256, 176);
-            source2 = new Rectangle(390, 192, 192, 112);
-            leftDoorPos = new Rectangle(848, 44, 32, 32);
-            topDoorPos = new Rectangle(815, 11, 32, 32);
-            bottomDoorPos = new Rectangle(815, 110, 32, 32);
-            rightDoorPos = new Rectangle(815, 77, 32, 32);
-            //source1 = new Rectangle(508, 840, 257, 168);
+            source1 = new Rectangle(508, 840, 257, 168);
             ////outer
             //source1 = new Rectangle(521, 11, 256, 176);
             ////room1
@@ -47,25 +39,13 @@ namespace sprint0
             //source6 = new Rectangle(815, 77, 32, 32);
 
 
-
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             //outer
-            spriteBatch.Draw(room, new Rectangle(0, 0, game1.GraphicsDevice.Viewport.Width, game1.GraphicsDevice.Viewport.Height), roomBorder, Color.White);
+            spriteBatch.Draw(room, new Rectangle(0, 0, game1.GraphicsDevice.Viewport.Width, game1.GraphicsDevice.Viewport.Height), source1, Color.White);
 
-            //top door
-            spriteBatch.Draw(room, new Rectangle(348, 0, 103, 90), topDoorPos, Color.White);
-
-            //left door
-            spriteBatch.Draw(room, new Rectangle(0, 195, 103, 90), leftDoorPos, Color.White);
-
-            //bottom door
-            spriteBatch.Draw(room, new Rectangle(348, 390, 103, 90), bottomDoorPos, Color.White);
-
-            //right door
-            spriteBatch.Draw(room, new Rectangle(700, 195, 103, 90), rightDoorPos, Color.White);
             ////top door
             //spriteBatch.Draw(room, new Rectangle(348, 0, 103, 90), source4, Color.White);
 
