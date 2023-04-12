ffff81f44c611afaa6d1c948c3d3d6442873b7d3
Afrah Taj <afrah.taj22@gmail.com>
2023-04-12 15:13:57 -0400

    ..

--- sprint0/Game1.cs
+++ sprint0/Game1.cs
@@ -17,6 +17,7 @@ using sprint0.Content;
 using sprint0.Interfaces;
 using sprint0.Items;
 using sprint0.HealthBar;
+using sprint0.Sound;
 using static System.Formats.Asn1.AsnWriter;
 using Vector2 = Microsoft.Xna.Framework.Vector2;
 
@@ -104,6 +105,7 @@ namespace sprint0
        // RoomsRoom currentRoom;
 
         private Song backgroundMusic;
+        private SoundClass sound;
 
         public Game1()
         {
@@ -112,8 +114,6 @@ namespace sprint0
             IsMouseVisible = true;
         }
 
-
-
         protected override void Initialize()
         {
             controller = new List<Icontroller>();
@@ -121,6 +121,8 @@ namespace sprint0
             pos = new Vector2(220, 100);
             healthNum = 6;
             healthbar = new Health(healthNum);
+
+
             base.Initialize();
 
             //int blockPosition1 = 45;
@@ -272,12 +274,15 @@ namespace sprint0
                 controller.Update(gameTime);
             }
 
-           // roomsroom.Update(gameTime, this);
+
+            currentRoom = ListOfRooms[doorEnter.currentImageIndex];
+            currentRoom.Update(gameTime, this);
+
+            // roomsroom.Update(gameTime, this);
 
             enemy.Update(gameTime, this);
             rooms.Update(gameTime);
             doorEnter.Update(gameTime, this);
-            currentRoom.Update(gameTime, this);
             item.Update(gameTime);
             blocks.Update(gameTime);
             projectiles.Update(gameTime);
