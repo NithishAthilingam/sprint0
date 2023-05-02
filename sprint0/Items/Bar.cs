1878ec9b27fa077a0f1be6f9dd75177608737a71
LauDaWei <lau.293@buckeyemail.osu.edu>
2023-05-02 00:21:30 -0400

    bar

--- sprint0/Items/Bar.cs
+++ sprint0/Items/Bar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace sprint0
 {
@@ -10,6 +11,8 @@ namespace sprint0
         Rectangle barS;
         Rectangle barD;
         Texture2D barDraw;
+        Rectangle link;
+        Boolean intersect;
 
         public Bar(Texture2D arrowSprite, Vector2 pos)
         {
@@ -17,6 +20,7 @@ namespace sprint0
             barD = new Rectangle((int)pos.X, (int)pos.Y, 75, 75);
 
             barDraw = arrowSprite;
+            intersect = false;
 
         }
 
@@ -27,7 +31,12 @@ namespace sprint0
 
         public void Update(GameTime gameTime, Game1 game)
         {
-        
+            link = new Rectangle((int)game.controller[0].GetLinkPos().X, (int)game.controller[0].GetLinkPos().Y, 30, 30);
+            if (link.Intersects(barD))
+            {
+                intersect = true;
+
             }
         }
     }
+}
